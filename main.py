04caaabc46f44fec86ba506a44de453c5d9c8858
Faisal Alzhrani | فيصل الزهراني <faisalmathandcs@gmail.com>
2024-08-28 14:28:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,52 @@
 from typing import Optional
-
 from fastapi import FastAPI
+
 
 app = FastAPI()
 
+@app.get('/')
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def root():
+    return {"message": "We ARE APIERS"}
+
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+async def read_item(item_id):
+    return {"item_id": item_id}
+
+
+import joblib 
+
+model = joblib.load("Log_Reg.joblib")
+scaler = joblib.load("Scaler.joblib")
+
+from pydantic import BaseModel
+
+from pydantic import BaseModel
+
+class InputFeatures(BaseModel):
+        Mileage: int
+        Engine_Size: float
+    
+
+def preprocessing(input_features: InputFeatures):
+    dict_f = {
+        'Mileage': input_features.Mileage,
+        'Engine_Size': input_features.Engine_Size
+    
+    }
+    feature_list = [dict_f[key] for key in sorted(dict_f)]
+    return scaler.transform([list(dict_f.values())])
+    
+
+@app.get("/predict")
+def predict(input_features: InputFeatures):
+    return preprocessing(input_features)
+
+@app.post("/predict")
+
+async def predict(input_features: InputFeatures):
+    data = preprocessing(input_features)
+    y_pred = model.predict(data)
+    return {'pred': y_pred.tolist()[0]}
+
